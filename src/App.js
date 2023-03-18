04d2819c5807a0d9e19634f89591fb3847946f31
John Doe <jkim458@my.centennialcollge.ca>
2023-03-18 15:20:15 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+//
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
+//
+import List from './components/List';
+import EditStudent from './components/EditStudent';
+import EditCourse from './components/EditCourse';
 
+import CreateStudent from './components/CreateStudent';
+import CreateCourse from './components/CreateCourse';
+
+import ShowStudent from './components/ShowStudent';
+import ShowCourse from './components/ShowCourse';
+
+import Home from './components/Home';
+import Login from './components/Login';
+//
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/list">List of Students</Nav.Link>
+            <Nav.Link href="/create">Sign Up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    
+      <div>          
+          <Route render ={()=> < Home />} path="/home" />
+          <Route render ={()=> < Login />} path="/login" />
+          <Route render ={()=> < List />} path="/list" />
+          <Route render ={()=> < EditStudent />} path="/edit/:id" />
+          <Route render ={()=> < CreateStudent />} path="/create" />
+          <Route render ={()=> < ShowStudent />} path="/show/:id" />
+          <Route render ={()=> < ShowCourse />} path="/showcourse/:id" />
+          <Route render ={()=> < EditCourse />} path="/editcourse/:id" />
+          <Route render ={()=> < CreateCourse />} path="/createcourse" />
+
+      </div>
+
+    </Router>
+
+
   );
 }
-
+//<Route render ={()=> < App />} path="/" />
 export default App;
